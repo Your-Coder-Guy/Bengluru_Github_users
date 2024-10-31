37275277022dc46c9a5fb226d0811e835dd55363
Your Coder Guy <142489020+Your-Coder-Guy@users.noreply.github.com>
2024-10-31 20:22:49 +0530

    Delete analyse.py

--- analyse.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-repo = pd.read_csv('repositories.csv')
-
-# Count the number of repositories per language
-
-languages_counts = repo['language'].value_counts()
-print(languages_counts)
